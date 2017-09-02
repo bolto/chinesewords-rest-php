afeb29a39b6e7b0556fc3f2034f261eb129b4402
Annon Hong <annonhong@gmail.com>
2017-09-01 22:30:04 -0700

    REST route and converter example working

--- web/index.php
+++ web/index.php
@@ -7,16 +7,16 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = require __DIR__.'/../src/app.php';
 
 //annon's code
+//note the ->convert() code is used to
+//apply conversion to input parameters like $id or whatever
+//this is good as the converter can be of its own method
+//and actual json response code does not need to be concerned
+//with conversion.  this is good abstraction.
 $app['debug'] = true;
-$app->get('/test', function () use ($blogPosts) {
-    $output = '';
-    foreach ($blogPosts as $post) {
-        $output .= $post['title'];
-        $output .= '<br />';
-    }
+$app->get('/test/{id}', function ($id){
 
-    return $output;
-});
+    return "hello: $id.";
+})->convert('id', function ($id) { return (int) $id + 1; });
 
 require __DIR__.'/../config/prod.php';
 require __DIR__.'/../src/controllers.php';
