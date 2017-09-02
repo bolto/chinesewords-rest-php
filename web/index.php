6e6b8684e71704f704e88ece6ca52dc98895ea95
Annon Hong <annonhong@gmail.com>
2017-09-01 22:44:24 -0700

    Define abstracted  converter function

--- web/index.php
+++ web/index.php
@@ -6,6 +6,14 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../src/app.php';
 
+$userIdConverter = function ($id) {
+    // you can easily define any converter
+    // and assign it to a variable,
+    // then pass this variable to be used
+    // in the converter.
+    return (int)$id + 2;
+};
+
 //annon's code
 //note the ->convert() code is used to
 //apply conversion to input parameters like $id or whatever
@@ -14,9 +22,11 @@ $app = require __DIR__.'/../src/app.php';
 //with conversion.  this is good abstraction.
 $app['debug'] = true;
 $app->get('/test/{id}', function ($id){
-
+    // note the function ($id) can also
+    // be defined to cast input variable to a class instance
+    // eg: function (User $user)
     return "hello: $id.";
-})->convert('id', function ($id) { return (int) $id + 1; });
+})->convert('id', $userIdConverter);
 
 require __DIR__.'/../config/prod.php';
 require __DIR__.'/../src/controllers.php';
